340c098248585e97448d357806a651ce2f5361b4
eggshell <edzeljohnsinfuego@gmail.com>
2023-08-29 12:11:13 +0800

    add links in the lower sidebar

--- patients/appointments.php
+++ patients/appointments.php
@@ -14,6 +14,20 @@ if($_SESSION['usertype'] != 'p'){
 
     
 ?>
+<script>
+//ajax script to cancel appointment
+function cancel_appointment(id){
+    $.ajax({
+        url: 'cancel_appointment.php',
+        type: 'POST',
+        data: {appointment_id: id},
+        success: function(response){
+            alert(response);
+            location.reload();
+        }
+    });
+}
+</script>
 <div class="col" style="background: #f1f0f0;font-family: Montserrat, sans-serif;margin-left: 24px;border-radius: 10px;padding-top: 9px;padding-left: 15px;padding-right: 18px;">
     <h1 style="font-family: Montserrat, sans-serif;padding: 17px;padding-top: 20px;margin-left: 150px;margin-right: -3px;padding-left: 20px;padding-right: 20px;border-radius: 10px;background: #f1f0f0;margin-top: 10px;">Appointments Schedule</h1>
     <div class="table-responsive" style="font-family: Alatsi, sans-serif;text-align: left;--bs-body-bg: var(--bs-primary-bg-subtle);--bs-body-font-weight: normal;border-radius: 15px;padding-right: 0px;background: #f1f0f0;">
@@ -38,7 +52,6 @@ if($_SESSION['usertype'] != 'p'){
                     $appointment['stat'] = ucfirst($appointment['stat']);
                     $appointment['time'] = date('h:i A', strtotime($appointment['time']));
                     
-                
                     echo "<tr style='border-style: solid;background: rgba(255,255,255,0);'>";
                     echo "<td style='font-family: Montserrat, sans-serif;border-width: 1px;border-style: solid;background: rgba(255,255,255,0); font-weight: bold;'>".$patient_name."</td>";
                     echo "<td style='font-family: Montserrat, sans-serif;border-width: 1px;border-style: solid;background: rgba(255,255,255,0);'>".$appointment['type']."</td>";
@@ -48,7 +61,7 @@ if($_SESSION['usertype'] != 'p'){
                     </td>
                     <td style='font-family: Montserrat, sans-serif;border-width: 1px;border-style: solid;background: rgba(255,255,255,0);'>
                     <a href='edit_appointment.php' class='btn btn-primary btn-sm' type='button' style='background: #2ecc71;border-style: none;'>Update</a>
-                    <a href='cancellation.php' class='danger btn-sm' type='button' style='border-style: none; margin-left: 10px;'>Cancel</a>
+                    <a href='' onClick='cancel_appointment()' id='cancel' class='danger btn-sm' type='button' style='border-style: none; margin-left: 10px;'>Cancel</a>
                     <input type='checkbox' name='appointment_ids[]' value=".$appointment['patient_id']." style='margin-left: 20px;'></td>
                     </td>";
                     echo "</tr>";
@@ -57,6 +70,6 @@ if($_SESSION['usertype'] != 'p'){
         </table>
     </div>
 </div>
-        </div>
+    </div>
     </div>
 <?php  include(__DIR__ . '/../_footer.php'); ?>
